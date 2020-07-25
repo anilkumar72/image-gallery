ca6ea2350ec489fe490a77490fda121af3b289a6
anilkumar72 <anilkumarghanta7@gmail.com>
2020-07-26 03:32:32 +0530

    first commit

---
+++ src/api/Unsplash.js
@@ -0,0 +1,1 @@
+const API_LINK =`https://api.unsplash.com/photos/?client_id=QIetJ5YSd8XkQx17kz0QR5f_PS-80hMcL8lwHLtblUA`;
---
+++ src/components/section/OverLay.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Wrapper = styled.section`
+
+`;
+
+export default () => {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    )
+}
